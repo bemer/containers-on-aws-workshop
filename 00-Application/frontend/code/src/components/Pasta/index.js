76de369b8a0756b99a77e22b482bdfb695afa4a9
claick-oliveira <claick.cao@gmail.com>
2018-11-08 19:31:20 -0200

    Fix word pasta

--- 00-Application/frontend/code/src/components/Pasta/index.js
+++ 00-Application/frontend/code/src/components/Pasta/index.js
@@ -8,7 +8,7 @@ const Pasta = ({ handleVote, isVote, voteCount }) => (
       <div className="hovereffect pasta">
         <img className="img-responsive pasta" src={PastaGif} alt=""/>
         <div className="overlay pasta">
-          <h2>Chicken</h2>
+          <h2>Pasta</h2>
           <p><a href="#" onClick={handleVote.bind(this, 'Pasta')}>VOTE</a></p> 
         </div>
       </div>
